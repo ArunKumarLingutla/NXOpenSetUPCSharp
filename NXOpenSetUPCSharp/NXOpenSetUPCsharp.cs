1d87a18c803b72bc822e39ec96ab0031022d1eef
Arun Kumar Lingutla <arunkumarlingutla94177@gmail.com>
2025-03-20 12:48:20 +0530

    with load unload methods, session parts

--- NXOpenSetUPCSharp/NXOpenSetUPCsharp.cs
+++ NXOpenSetUPCSharp/NXOpenSetUPCsharp.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NXOpen;
+using NXOpen.UF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,50 @@ namespace NXOpenSetUPCSharp
 {
     public class NXOpenSetUPCsharp
     {
+        //class members
+        private static NXOpen.Session theSession = null;
+        private static NXOpen.UF.UFSession theUFSession = null;
+        private static NXOpen.UI theUI = null;
 
+        public static void Main(string[] args)
+        {
+            try
+            {
+                theSession = Session.GetSession();
+                theUFSession = NXOpen.UF.UFSession.GetUFSession();
+                theUI = NXOpen.UI.GetUI();
+                NXOpen.Part workPart = theSession.Parts.Work;
+                NXOpen.Part displayPart = theSession.Parts.Display;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static int GetUnloadOption(string arg)
+        {
+            //return System.Convert.ToInt32(Session.LibraryUnloadOption.Explicitly);
+            return System.Convert.ToInt32(Session.LibraryUnloadOption.Immediately);
+            // return System.Convert.ToInt32(Session.LibraryUnloadOption.AtTermination);
+        }
+
+        //------------------------------------------------------------------------------
+        // Following method cleanup any housekeeping chores that may be needed.
+        // This method is automatically called by NX.
+        //------------------------------------------------------------------------------
+        public static void UnloadLibrary(string arg)
+        {
+            try
+            {
+                //---- Enter your code here -----
+            }
+            catch (Exception ex)
+            {
+                //---- Enter your exception handling code here -----
+                theUI.NXMessageBox.Show("Main", NXMessageBox.DialogType.Error, ex.ToString());
+            }
+        }
     }
 }
